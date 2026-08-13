e6bab226542bc004a3e49182dd9acba102de92b1
Prashant <pk.ashu98@gmail.com>
2026-08-13 23:48:53 +0530

    accept/reject connection request

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,6 +43,9 @@ const NavBar = () => {
                             </Link>
                         </li>
                         <li><Link to="/connections">Connections</Link></li>
+                        <li>
+                            <Link to="/requests">Requests</Link>
+                        </li>
                         <li><a onClick={handleLogout}>Logout</a></li>
                     </ul>
                 </div>
